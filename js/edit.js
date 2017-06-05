587efa265bec8ee1c7ed1aca2e4e78e89a89833c
tobias brunner <tobiasbrunner@tobiass-MBP.fritz.box>
2017-06-05 20:36:07 +0200

    handlebars2

--- js/edit.js
+++ js/edit.js
@@ -1,30 +1,73 @@
 /**
  * Created by tobiasbrunner on 21.05.17.
  */
+let render = function(toDo) {
+    var editTemplate = $('#editTemplate').html();
+    var createEditHtml = Handlebars.compile(editTemplate);
+    $("body").html(createEditHtml(toDo));
+    if (toDo.ratingSystem) {
+        $("#bolt" + toDo.ratingSystem)[0].checked = true;
+    }
+};
 
+let getToDo = function(id){
+    var toDos = getToDos();
+    var toDo = toDos.filter(function( obj ) {
+        return obj.id == id;c
+    })[0];
+    return toDo ? toDo : [];
+};
+
+let getToDos = function() {
+    var lsToDos = localStorage.getItem("toDos");
+    return lsToDos ? JSON.parse(lsToDos) : [];
+};
+
+let setToDo = function(note){
+    var toDos = getToDos()
+    var filteredToDos = toDos.filter(function( obj ) {
+        return obj.id != note.id;
+    });
+    filteredToDos.push(note);
+    localStorage.setItem("toDos", JSON.stringify(filteredToDos));
+};
+
+let getNextId = function(){
+    var toDos = getToDos();
+
+    return toDos.reduce(function(prevVal, elem) {
+        return elem.id >= prevVal ? elem.id + 1 : prevVal;
+    }, 0);
+}
 
 $( document ).ready(function() {
 
+
+    var id;
+    if(window.location.hash) {
+        id = window.location.hash.substring(1);
+        var toDo = getToDo(id);
+        render(toDo);
+
+    } else {
+        render();
+    }
+
     $("#btnCancel").click(function () {
-        console.log("cancel");
+        window.location.replace("index.html");
     });
 
     $("#btnSave").click(function () {
-        console.log("save");
-
+        id = id ? id : getNextId();
+        console.log(id);
         var newToDo = {};
         newToDo["title"] = document.getElementById("title").value;
         newToDo["description"] = document.getElementById("description").value;
         newToDo["ratingSystem"] = $('input[type="radio"]:checked').val();
-        newToDo["dueDate"] = document.getElementById("dueDate").value;
-        console.log(newToDo);
+        newToDo["dueDate"] = $("#dueDate").val();
+        newToDo["id"] = id;
 
-        var lsToDos = localStorage.getItem("toDos");
-        var toDos = lsToDos ? JSON.parse(lsToDos) : [];
-
-        toDos.push(newToDo);
-        localStorage.setItem("toDos", JSON.stringify(toDos));
-
+        setToDo(newToDo);
         window.location.replace("index.html");
 
     });
--- js/index.js
+++ js/index.js
@@ -4,15 +4,91 @@
 
 $(document).ready(function () {
 
-    var lsToDos = localStorage.getItem("toDos");
-    var toDos = lsToDos ? JSON.parse(lsToDos) : [];
 
-    document.getElementById("anzahlEintraege").innerHTML = toDos.length;
-    console.log(toDos.length);
-    for (i in toDos){
-        console.log(toDos[i]);
+    Handlebars.registerHelper('times', function(n) {
+        var accum = '';
+        for(var i = 0; i < n; ++i)
+            accum += "\2607";
+        return accum;
+    });
 
+    Handlebars.registerHelper('formatDate', function(datetime) {
+        if (moment) {
+            return moment(datetime).fromNow();
+        }
+        else {
+            return datetime;
+        }
+    });
+
+    let getToDos = function(){
+        var lsToDos = localStorage.getItem("toDos");
+        return lsToDos ? JSON.parse(lsToDos) : [];
     }
+
+    let setToDos = function(toDos){
+        localStorage.setItem("toDos", JSON.stringify(toDos));
+        var lsToDos = localStorage.getItem("toDos");
+        return lsToDos ? JSON.parse(lsToDos) : [];
+    }
+
+    let render = function() {
+
+        var toDos = getToDos();
+        console.log(toDos);
+        document.getElementById("anzahlEintraege").innerHTML = toDos.length;
+
+        if (!$("#cbxFinished")[0].checked) {
+            toDos = toDos.filter(function(obj){
+                return !obj.finished;
+            })
+        }
+        console.log("neu:", toDos);
+
+        var toDosTemplate = $('#toDosTemplate').html();
+        var createToDosHtml = Handlebars.compile(toDosTemplate);
+        $("#notes").html(createToDosHtml(toDos));
+        for (let elem of toDos){
+            $("#cbx" + elem.id).on('change', function(){
+                    updateNote(elem.id, this.checked)
+                }
+            );
+        }
+    };
+
+
+    let updateNote = function(id, checked){
+        var toDos = getToDos();
+        var currToDo = toDos.filter(function( obj ) {
+            return obj.id == id;
+        })[0];
+        if (checked) {
+            currToDo.finished = true;
+        } else {
+            currToDo.finished = false;
+        }
+        var newToDos = toDos.filter(function( obj ) {
+            return obj.id != id;
+        });
+        newToDos.push((currToDo));
+        setToDos(newToDos);
+        render()
+    };
+
+    $("#btnCreatedDate").click( function() {
+            var toDos = getToDos();
+
+            setToDos(toDos.sort(function (a, b) {
+                return moment(a.dueDate) - moment(b.dueDate);
+            }));
+            render();
+        }
+    );
+    $("#cbxFinished").change(function(){
+        render();
+    })
+    //get data attribute
+    render();
 
 
 })
